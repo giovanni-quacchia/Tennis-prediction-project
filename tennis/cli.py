bb456b7fce3e0b86a9bd9970122e107928e882a9
giovanni-quacchia <giovanni.quacchia@gmail.com>
2026-04-28 10:18:16 +0200

    small update on splitting training and testing sets

--- tennis/cli.py
+++ tennis/cli.py
@@ -4,6 +4,7 @@ import pandas as pd
 from tennis.models.knn import TennisKNN
 from tennis.data_loader import prepare_data as prepare_data_func
 from tennis.config import DataConfig, TrainingConfig, FeatureConfig
+from sklearn.model_selection import train_test_split
 
 app = typer.Typer()
 
@@ -25,16 +26,29 @@ def prepare_data():
 @app.command()
 def train_knn():
 
-    config, training_config = DataConfig(), TrainingConfig()
+    config = DataConfig()
+    training_config = TrainingConfig()
     model = TennisKNN(training_config.knn)
+    features = FeatureConfig()
 
-    training_set = pd.read_excel(config.prepared_data_path)
+    df = pd.read_excel(config.prepared_data_path)
 
+    # Split the data into training and testing sets
+    train_set, test_set = train_test_split(df, 
+                            test_size=training_config.test_size, 
+                            random_state=training_config.random_state)
+
+    # Train the model and tune hyperparameters
     typer.echo("Training KNN model...")
-    params, accuracy = model.train(training_set)
+    params, accuracy = model.train(train_set)
 
     typer.echo(f"Best hyperparameters: {params}")
-    typer.echo(f"Cross-validation accuracy: {accuracy:.2%}")
+    typer.echo(f"Training accuracy: {accuracy:.2%}")
+
+    # Test the model on the testing set
+    X_test, y_test = test_set[features.trainable], test_set["y"]
+    test_accuracy = model.fitted_pipeline.score(X_test, y_test)
+    typer.echo(f"Testing accuracy: {test_accuracy:.2%}")
 
     typer.echo("Training completed. Saving the model...")
     # Save the model for future tests
@@ -43,7 +57,7 @@ def train_knn():
 @app.command()
 def predict_knn():
 
-    config, training_config = DataConfig(), TrainingConfig()
+    config = DataConfig()
     features = FeatureConfig()
 
     pipeline = joblib.load(config.knn_model_path)
--- tennis/config.py
+++ tennis/config.py
@@ -67,7 +67,7 @@ class RandomForestConfig(BaseModel):
     bootstrap: bool = True
 
 class TrainingConfig(BaseModel):
-    test_size: float = 0.3
+    test_size: float = 0.2
     random_state: int = 42
 
     knn: KNNConfig = KNNConfig()
